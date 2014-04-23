9497ba41b99ee5a037a3eeaa2bb8e57f2688f807
Kyle Craft <craftjk@gmail.com>
2014-04-23 00:36:15 -0700

    _. filter now passing tests

--- src/underbar.js
+++ src/underbar.js
@@ -84,6 +84,13 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+		var passes = [];
+		_.each(collection, function(item, index) {
+			if (test(item)) {
+				passes.push(item);
+			}
+		});
+		return passes;
   };
 
   // Return all elements of an array that don't pass a truth test.
