91256b780930d9148ce1c4cb8353741752b65456
Kyle Craft <craftjk@gmail.com>
2014-04-26 15:32:43 -0700

    _.extend now passing specs

--- src/underbar.js
+++ src/underbar.js
@@ -249,17 +249,21 @@ var _ = {};
   // object(s).
   //
   // Example:
-  //   var obj1 = {key1: "something"};
-  //   _.extend(obj1, {
-  //     key2: "something new",
-  //     key3: "something else new"
-  //   }, {
-  //     bla: "even more stuff"
-  //   }); // obj1 now contains key1, key2, key3 and bla
+    // var obj1 = {key1: "something"};
+    // _.extend(obj1, {
+    //   key2: "something new",
+    //   key3: "something else new"
+    // }, {
+    //   bla: "even more stuff"
+    // }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-		for (var i = 0 ; i < arguments.length ; i++) {
-			obj[key] = value;
+		for (var i = 1 ; i < arguments.length ; i++) {
+			for (var key in arguments[i]) {
+				obj[key] = arguments[i][key];
+			}
 		}
+		
+		return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
